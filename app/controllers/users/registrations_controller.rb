fe1705e9a2402804c335fce59a0e1a7376a1b23c
H4K1 <71144997+H4K1@users.noreply.github.com>
2022-04-08 21:02:29 -0400

    Add extra strong params for user registrations

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,7 +3,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   layout 'devise/layout'
 
-  # before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -43,9 +43,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[nickname name])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
